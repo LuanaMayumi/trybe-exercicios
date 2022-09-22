f51a1484993cc6021e17ccffba450df432f774dc
LuanaMayumi <luh.mayumi@gmail.com>
2022-09-21 22:07:06 -0300

    exercicio 1 OK

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-intoducao-javascript/scripts-exercicios-propostos.js
@@ -0,0 +1,65 @@
+//EXERCICIO 1:
+
+const a = 2;
+const b = 6;
+
+let adição = a+b;
+console.log(adição)
+
+let subtração = a-b;
+console.log(subtração)
+
+let muntiplicação = a*b;
+console.log(muntiplicação)
+
+let divisão = a/b;
+console.log(divisão)
+
+let módulo = a % b;
+console.log(módulo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let angulo1 = 100;
+let angulo2;
+let angulo3;
+
+//a soma deve ser 180 graus
+
+
+// if (angulo1 + angulo2 + angulo3 === 180){
+//     console.log('true');
+//     }
+//     else {
+// console.log('false');    
+//     }
+
+
+    const anguloUm = 23;
+    const anguloDois = 53;
+    const anguloTres = -100;
+
+    if (anguloUm + anguloDois + anguloTres === 180) {
+console.log('true');
+    } else if (anguloUm + anguloDois + anguloTres !== 180) {
+console.log('false')
+    } else if (anguloUm || anguloDois || anguloTres <0) {
+        console.log('erro');
+    }
+       
+    
+// console.log(anguloUm - anguloDois - anguloTres);
+
