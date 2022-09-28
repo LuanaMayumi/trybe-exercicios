6ee191e81e0cea3c764d6dcf9313e65438ba1745
LuanaMayumi <luh.mayumi@gmail.com>
2022-09-28 20:36:25 -0300

    resolvendo ex 2 e 3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-JavaScript-DOM-e-seletores/exercicios-propostos-aula.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-JavaScript-DOM-e-seletores/exercicios-propostos-aula.js
@@ -3,4 +3,12 @@ function mudarOTexto(texto) {
   } 
   mudarOTexto('texto');
 
-  
+  function changeBackground (colorBack){
+    document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+  }
+  changeBackground('colorBack');
+
+  function changeBackground (colorBackDois){
+    document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+  }
+  changeBackground('colorBackDois');
